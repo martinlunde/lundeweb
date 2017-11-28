f147f4d96385c8761136b2d3ea5df99927a30f3b
martinlunde <martlun@stud.ntnu.no>
2017-11-28 20:31:09 +0100

    Add text to homepage and add scaling

--- src/views/containers/Home.js
+++ src/views/containers/Home.js
@@ -5,7 +5,22 @@ export class Home extends React.Component {
     render() {
         return (
             <div>
-                <HexagonGrid />
+                <div className="HomePageTextWrapper">
+                    <div className="HomePageTitle">
+                        Lundeweb
+                    </div>
+                    <div className="HomePageText">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna
+                        aliqua. Nunc sed velit dignissim sodales ut. Accumsan in
+                        nisl nisi scelerisque eu ultrices vitae auctor eu. Aenean
+                        pharetra magna ac placerat vestibulum lectus mauris.
+                        Massa tincidunt nunc pulvinar sapien et ligula ullamcorper
+                    </div>
+                </div>
+                <div className="HexagonGridWrapper">
+                    <HexagonGrid />
+                </div>
             </div>
         );
     };
